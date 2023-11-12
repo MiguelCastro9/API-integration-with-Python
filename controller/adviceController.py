004cbaecb571d1774df28202c0b67bc1663ee3d5
MiguelCastro9 <miguel.castro9@hotmail.com>
2023-11-12 12:12:16 -0300

    add routes request from flask

--- controller/adviceController.py
+++ controller/adviceController.py
@@ -11,8 +11,8 @@ def get_advice():
         phrase_id = data_json['slip']['id']
         phrase_advice = data_json['slip']['advice']
         object_advice = Advice(phrase_id, phrase_advice)
-        print(object_advice.phrase_id)
-        print(object_advice.phrase_advice)
+        print("ID:", object_advice.phrase_id)
+        print("Advice:", object_advice.phrase_advice)
         return object_advice
     else:
         print(f"request falied: {request.status_code}")
--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from controller.adviceController import get_advice
+from flask import Flask, jsonify
+ 
+app = Flask(__name__)
 
-get_advice()
+@app.route('/advices', methods=['GET'])
+def advices():
+    advice = get_advice()
+    if advice:
+        return jsonify({"ID": advice.phrase_id, "Advice": advice.phrase_advice})
+    else:
+        return jsonify({"error": "request falied"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
